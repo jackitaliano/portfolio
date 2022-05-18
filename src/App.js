eadb8a2b4654c8f4c0450327c505969e6fdea707
jackitaliano <italiano.16@osu.edu>
2022-05-18 15:23:09 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import { Navbar } from "./components";
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <div>
         <Routes>
           <Route path="/" element={"test"}></Route>
