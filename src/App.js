9c644ae04e2a9f8cdb98d71a06af8cc5ea02836b
jackitaliano <italiano.16@osu.edu>
2022-05-18 17:56:45 -0500

    added about page to route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Navbar } from "./components";
+import { About } from "./pages";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Navbar />
       <div>
         <Routes>
-          <Route path="/" element={"test"}></Route>
+          <Route path="/" element={<About />}></Route>
         </Routes>
       </div>
     </Router>
