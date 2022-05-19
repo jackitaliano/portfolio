a572c20f12a3c039fcd2b52391610216d406e2ec
jackitaliano <italiano.16@osu.edu>
2022-05-18 23:42:30 -0500

    added exports for links, header, and hello

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 export { default as Navbar } from "./navbar/Navbar";
+export { default as Links } from "./links/Links";
+export { default as Header } from "./header/Header";
+export { default as Hello } from "./hello/Hello";
