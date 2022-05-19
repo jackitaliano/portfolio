62af509f1fcf8ab2747927df4f3a470a216cdb05
jackitaliano <italiano.16@osu.edu>
2022-05-18 23:44:27 -0500

    moved header and hello to separate components

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,48 +1,12 @@
 import React from "react";
 import "./about.css";
-
-import { FaGithub, FaLinkedin, FaTwitter, FaDownload } from "react-icons/fa";
-import Profile from "../../assets/profile.jpg";
+import { Header, Hello } from "../../components";
 
 const About = () => {
   return (
-    <div className="section bg-c-accent">
-      <div className="row">
-        {/* Profile Image */}
-        <div className="col-md-4">
-          <img src={Profile} className="about-profile-img" alt="profile pic" />
-        </div>
-
-        {/* Information */}
-        <div className="col-md-5">
-          <h1>Jack Italiano</h1>
-          <h5>Student at The Ohio State University</h5>
-          <h5>Front-end Developer</h5>
-          <h5>italiano.16@osu.edu</h5>
-          <h5>(847) 477-8066</h5>
-        </div>
-
-        {/* Links */}
-
-        <div className="col-md-3">
-          <div className="col ms-auto">
-            <div className="row-md-3 ms-auto">
-              <a href="https://www.linkedin.com/in/jackitaliano/">
-                <FaLinkedin />
-              </a>
-            </div>
-            <div className="row-md-3">
-              <FaGithub />
-            </div>
-            <div className="row-md-3">
-              <FaTwitter />
-            </div>
-            <div className="row-md-3">
-              Resume <FaDownload />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Header />
+      <Hello />
     </div>
   );
 };
