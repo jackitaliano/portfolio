7f5eea98e6d24370f502d589489cb22f966bcd3f
italiano16 <italiano.16@osu.edu>
2022-05-25 15:27:16 -0500

    added project import

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from "./navbar/Navbar";
 export { default as Links } from "./links/Links";
 export { default as Header } from "./header/Header";
 export { default as Hello } from "./hello/Hello";
+export { default as Project } from "./project/Project.jsx";
