ccc5a700f518cc79f2715677337255424c6276d3
italiano16 <italiano.16@osu.edu>
2022-05-25 15:28:34 -0500

    moved navbar into header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,48 +1,40 @@
 import React from "react";
 import { Links } from "../../components";
 
+import "./header.css";
+
 import Profile from "../../assets/profile.jpg";
-import BackgroundImg from "../../assets/oval-pano.png";
+import { Navbar } from "../../components";
 
 const Header = () => {
-  const styles = {
-    header: {
-      backgroundImage: `url(${BackgroundImg})`,
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-    },
-    content: {
-      backgroundColor: "rgba(0,0,0,0.75)",
-    },
-  };
   return (
-    <div style={styles.header}>
-      <div
-        className="section bg-c-accent d-flex flex-column flex-md-row justify-content-between"
-        style={styles.content}
-      >
-        <div className="d-flex flex-column flex-md-row">
-          {/* Profile Image */}
-          <div className="d-none d-sm-block me-3 mb-5 mb-md-0">
-            <img
-              src={Profile}
-              className="about-profile-img"
-              alt="profile pic"
-            />
+    <div className="bg-image">
+      <div className="bg-tint">
+        <Navbar />
+        <div className="header-container d-flex flex-column flex-sm-row">
+          <div className="me-auto d-flex flex-column flex-md-row">
+            {/* Profile Image */}
+            <div className="d-none d-md-block me-4 mb-4 mb-md-0">
+              <img
+                src={Profile}
+                className="about-profile-img"
+                alt="profile pic"
+              />
+            </div>
+
+            {/* Information */}
+            <div className="sub-heading mb-4 mb-md-0">
+              <h1 className="main-heading mb-md-5">Jack Italiano</h1>
+              <h5 className="sub-heading">CSE at The Ohio State University</h5>
+              <h5>Front-end Developer</h5>
+              <h5>italiano.16@osu.edu</h5>
+              <h5>(847) 477-8066</h5>
+            </div>
           </div>
 
-          {/* Information */}
-          <div className="mb-5 mb-md-0 text-info">
-            <h1 className="mb-md-5">Jack Italiano</h1>
-            <h5>Student at The Ohio State University</h5>
-            <h5>Front-end Developer</h5>
-            <h5>italiano.16@osu.edu</h5>
-            <h5>(847) 477-8066</h5>
-          </div>
+          {/* Links */}
+          <Links />
         </div>
-
-        {/* Links */}
-        <Links />
       </div>
     </div>
   );
