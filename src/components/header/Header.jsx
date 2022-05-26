0aedb7663cbfc27490beb096449411f46b11af0c
italiano16 <italiano.16@osu.edu>
2022-05-26 16:53:12 -0500

    removed navbar, moved links
    -fixed margin to account for now fixed navbar
    -links now at top on md/sm screens, still on bottom for lg screens

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,36 +4,38 @@ import { Links } from "../../components";
 import "./header.css";
 
 import Profile from "../../assets/profile.jpg";
-import { Navbar } from "../../components";
 
 const Header = () => {
   return (
-    <div className="bg-image">
-      <div className="bg-tint">
-        <Navbar />
-        <div className="header-container d-flex flex-column flex-sm-row">
-          <div className="me-auto d-flex flex-column flex-md-row">
-            {/* Profile Image */}
-            <div className="d-none d-md-block me-4 mb-4 mb-md-0">
-              <img
-                src={Profile}
-                className="about-profile-img"
-                alt="profile pic"
-              />
+    <div className="">
+      <div className="bg-image">
+        <div className="bg-tint">
+          <div className="header-fixed-nav header-container d-flex flex-row">
+            <div className="me-auto d-flex flex-column flex-md-row">
+              {/* Profile Image */}
+              <div className="d-none d-md-block me-4 mb-4 mb-md-0">
+                <img
+                  src={Profile}
+                  className="about-profile-img"
+                  alt="profile pic"
+                />
+              </div>
+
+              {/* Information */}
+              <div className="d-flex flex-column">
+                <h1 className="main-heading mb-4">Jack Italiano</h1>
+                <div className="sub-heading mt-auto">
+                  <h5>The Ohio State University CSE</h5>
+                  <h5>Front-end Developer</h5>
+                  <h5>italiano.16@osu.edu</h5>
+                  <h5>(847) 477-8066</h5>
+                </div>
+              </div>
             </div>
 
-            {/* Information */}
-            <div className="sub-heading mb-4 mb-md-0">
-              <h1 className="main-heading mb-md-5">Jack Italiano</h1>
-              <h5 className="sub-heading">CSE at The Ohio State University</h5>
-              <h5>Front-end Developer</h5>
-              <h5>italiano.16@osu.edu</h5>
-              <h5>(847) 477-8066</h5>
-            </div>
+            {/* Links */}
+            <Links />
           </div>
-
-          {/* Links */}
-          <Links />
         </div>
       </div>
     </div>
