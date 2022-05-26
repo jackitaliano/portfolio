e78e0f1c0c490bc168cbb233064521cc99c711c1
italiano16 <italiano.16@osu.edu>
2022-05-26 16:57:31 -0500

    moved navbar back to app for fixed navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import React from "react";
 
 import { About, Projects } from "./pages";
+import { Navbar } from "./components";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <About />
       <Projects />
     </div>
