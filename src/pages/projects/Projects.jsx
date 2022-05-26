0391e00bd4001ed75a5af9c7e9ce631897c2577c
italiano16 <italiano.16@osu.edu>
2022-05-26 17:02:20 -0500

    added ids for projects, removed container class

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { Project } from "../../components";
+import { ProjectCard } from "../../components";
 import image from "../../assets/profile.jpg";
 
 const Projects = () => {
   const projectsList = [
     {
+      id: "1",
       title: "project 1",
       text: "description",
       image: image,
       link: "",
-      technologies: [],
+      technologies: ["Bootstrap", "ReactJS", "NodeJS", "AWS"],
     },
     {
+      id: "2",
       title: "project 2",
       text: "description",
       image: image,
@@ -19,6 +21,7 @@ const Projects = () => {
       technologies: [],
     },
     {
+      id: "3",
       title: "project 3",
       text: "description",
       image: image,
@@ -27,11 +30,13 @@ const Projects = () => {
     },
   ];
 
-  const projects = projectsList.map((project) => <Project project={project} />);
+  const projects = projectsList.map((project) => (
+    <ProjectCard project={project} />
+  ));
   return (
-    <div className="container section" id="projects">
+    <div className="section" id="projects">
       <h1 className="main-heading">My Projects</h1>
-      {projects}
+      <div>{projects}</div>
     </div>
   );
 };
