17ba73a37f0543400c4da6d7e17bc71dadc8b874
italiano16 <italiano.16@osu.edu>
2022-06-02 19:53:01 -0500

    fixed "class", changed to "className"

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -4,11 +4,17 @@ import "./collapse.css";
 
 const Collapse = ({ id, text }) => {
   return (
-    <div class="collapse-chevron">
-      <div class="collapse" id={id}>
-        <p className="sub-text">{text}</p>
+    <div className="collapse-chevron">
+      <div className="collapse" id={id}>
+        <a
+          className="clickable text-decoration-none"
+          data-bs-toggle="collapse"
+          href={"#" + id}
+        >
+          <p className="sub-text">{text}</p>
+        </a>
       </div>
-      <a data-bs-toggle="collapse" class="collapsed" href={"#" + id}>
+      <a data-bs-toggle="collapse" className="collapsed" href={"#" + id}>
         <span aria-hidden="true">
           <FaChevronUp className="chevron" />
         </span>
