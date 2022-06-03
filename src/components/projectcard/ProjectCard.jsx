42b4beff09bcc0e511abc130b342f7dbe8c3c666
italiano16 <italiano.16@osu.edu>
2022-06-02 19:56:53 -0500

    changed format, added link
    changed format to have title, break, brief, break, technologies
    
    added github link

--- src/components/projectcard/ProjectCard.jsx
+++ src/components/projectcard/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./projectcard.css";
 import { TextCard, ProjectModal } from "../../components";
+import GithubLink from "../links/GithubLink";
 
 const ProjectCard = ({ project }) => {
   const technologies = project.technologies.map((tech) => (
@@ -9,31 +10,38 @@ const ProjectCard = ({ project }) => {
 
   return (
     <div
-      class="card mb-3 shadow clickable p-0"
+      className="card mb-3 shadow p-0 project-card"
       data-bs-toggle="modal"
       data-bs-target={"#project" + project.id}
     >
-      <div class="row g-md-0 g-3">
-        <div class="col-md-4 col-12">
+      <div className="row g-md-0 g-3">
+        <div className="col-md-4 col-12">
           <img
             src={project.image}
-            class="img-fluid card-img-dynamic"
+            className="img-fluid card-img-dynamic"
             alt="project"
           />
         </div>
-        <div class="col-md-8 col-12 d-flex">
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title main-heading">{project.title}</h5>
-            <p class="card-text sub-text">{project.text}</p>
+        <div className="col-md-8 col-12 d-flex">
+          <div className="card-body d-flex flex-column">
+            <div className="d-flex flex-row">
+              <h5 className="card-title sub-heading">{project.title}</h5>
 
-            <div className="mt-auto">
-              <hr />
-              <div className="d-flex flex-row">{technologies}</div>
+              <GithubLink link={project.link} />
+            </div>
+            <hr />
+            <p class="card-text sub-text project-description">
+              {project.brief}
+            </p>
+
+            <hr className="mt-auto" />
+            <div className="d-flex flex-row flex-wrap p-2 ps-3">
+              {technologies}
             </div>
           </div>
         </div>
       </div>
-      <ProjectModal project={project}></ProjectModal>
+      <ProjectModal className="d-none" project={project} />
     </div>
   );
 };
