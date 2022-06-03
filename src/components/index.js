b598974db74d18bbd0650af6b6b5933abca3cc60
italiano16 <italiano.16@osu.edu>
2022-06-02 19:51:50 -0500

    added GithubLink export

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as Links } from "./links/Links";
+export { default as GithubLink } from "./links/GithubLink";
 export { default as Header } from "./header/Header";
 export { default as Hello } from "./hello/Hello";
 export { default as ProjectCard } from "./projectcard/ProjectCard";
