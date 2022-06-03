b2470a3d58975d9b49604c361060de3022f2adc6
italiano16 <italiano.16@osu.edu>
2022-06-02 19:57:30 -0500

    implemented project modal to be similar to project card, but contains description instead of brief

--- src/components/projectmodal/ProjectModal.jsx
+++ src/components/projectmodal/ProjectModal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./projectmodal.css";
 
-import { TextCard } from "../../components";
+import { TextCard, GithubLink } from "../../components";
 
 const ProjectModal = ({ project }) => {
   const technologies = project.technologies.map((tech) => (
@@ -10,29 +10,38 @@ const ProjectModal = ({ project }) => {
 
   return (
     <div className="project-modal">
-      <div
-        class="modal fade"
-        id={"project" + project.id}
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">
-                {project.title}
-              </h5>
+      <div className="modal fade" id={"project" + project.id} tabindex="-1">
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <div className="d-flex">
+                <h5 className="modal-title me-3" id="projectModalTitle">
+                  {project.title}
+                </h5>
+                <GithubLink link={project.link} />
+              </div>
               <button
                 type="button"
-                class="btn-close project-modal-close"
+                className="btn-close project-modal-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body row">
-              <div className="col-12 col-md-4"></div>
-              <div class="modal-footer"></div>
+            <div className="modal-body row w-100 g-3 g-lg-0 bg-c-accent m-auto">
+              <a
+                className="img-fluid col-lg-5 col-12 h-fit"
+                href={project.link}
+                target="blank"
+                rel="noopener noreferrer"
+              >
+                <img src={project.image} className="img-fluid" alt="project" />
+              </a>
+              <p className="col-lg-7 col-12 sub-text px-3">
+                {project.description}
+              </p>
+            </div>
+            <div className="modal-footer">
+              <div className="d-flex flex-wrap">{technologies}</div>
             </div>
           </div>
         </div>
