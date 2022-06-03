8bf32d24e1bd8e0f94f1b32086fb3a9e887f4b4b
italiano16 <italiano.16@osu.edu>
2022-06-02 19:58:18 -0500

    changed margins, padding, and text alignment

--- src/components/textcard/TextCard.jsx
+++ src/components/textcard/TextCard.jsx
@@ -3,9 +3,9 @@ import "./textcard.css";
 
 const TextCard = ({ text }) => {
   return (
-    <div className="card text-card me-2">
-      <div className="card-body">
-        <div className="card-title sub-text">{text}</div>
+    <div className="card text-card me-2 mt-2 p-1">
+      <div className="card-body p-1 text-center align-middle">
+        <div className="card-title sub-text text m-auto">{text}</div>
       </div>
     </div>
   );
