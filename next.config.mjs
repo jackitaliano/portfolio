be1f5d6acc6eb503b001f8e749971390f1073ecb
jackitaliano <jitaliano.dev@gmail.com>
2024-09-28 16:25:12 -0400

    update build workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'dist'
+  basePath: "/portfolio",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
